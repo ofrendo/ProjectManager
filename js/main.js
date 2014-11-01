057035df6c06b8ba32231a7b13783a44b87840a5
Oliver <ofrendo@gmail.com>
2014-11-02 00:57:34 +0100

    da
    sd_ Please esäd.a#sä.da
    ter the commit message for your changes. Lines starting
    uit

--- js/main.js
+++ js/main.js
@@ -61,6 +61,7 @@ app.controller("main", ["$scope", "$http", "$timeout", "db", "ngDialog", functio
     ];
     $scope.itemInitialStatus = $scope.itemStatuses[2].text;
     $scope.$watch("itemStatuses", function(newStatus, oldStatus) {
+    	console.log("here");
     	localStorage.setItem(newStatus.lsID, newStatus.show)
     });
 
